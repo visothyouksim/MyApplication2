c0938cd221fb29eb8fe58dad7c2dcab85b63f65e
Visoth YOUK-SIM <115548934+visothyouksim@users.noreply.github.com>
2024-06-11 15:01:23 +0200

    update_v2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.test_jeudedes)
         val imageViewDice = findViewById<ImageView>(R.id.imageViewDice)
         val btnRollDice = findViewById<Button>(R.id.btn_roll_dice)
-        val tvResult = findViewById<TextView>(R.id.tv_result)
+        val tvResult = findViewById<TextView>(R.id.textViewDice)
 
         btnRollDice.setOnClickListener {
             val randomNumber = Random.nextInt(1, 7)
