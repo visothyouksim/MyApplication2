1d5b686225a54e1e6bead7054bf77b51821e303d
Visoth YOUK-SIM <115548934+visothyouksim@users.noreply.github.com>
2024-06-11 15:02:16 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.myapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
